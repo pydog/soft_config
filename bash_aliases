000a86fd99f177bfacbccb9f52c457537bb1a133
pydog <pydogwu@gmail.com>
2020-04-25 11:37:45 +0800

    add the alias cmd to save tmux buffer log

---
+++ bash_aliases
@@ -0,0 +1,1 @@
+alias savetmux="tmux capture-pane -pS -1000000 > $HOME/tmux.log"
--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 ln -s $PWD/tiny_vim/vimrc $HOME/.vimrc
 ln -s $PWD/gitconfig/gitconfig $HOME/.gitconfig
 ln -s $PWD/tmux_conf/tmux.conf $HOME/.tmux.conf
+ln -s $HOME/soft_config/bash_aliases $HOME/.bash_aliases
 git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
 
