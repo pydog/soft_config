7d86d38f441a173c50460444c88b731a1a792fa6
pydog <pydogwu@gmail.com>
2020-04-30 19:15:58 +0800

    update install.sh to install git-completion.bash and the \n\[\033[1;31m\]\u\[\033[35m\]@\[\033[1;32m\]\h\[\033[1;36m\]$

--- bash_aliases
+++ bash_aliases
@@ -1,2 +1,3 @@
 alias savetmux="tmux capture-pane -pS -1000000 > $HOME/tmux.log"
-export PS1=$PS1"\n\[\033[1;31m\]\u\[\033[35m\]@\[\033[1;32m\]\h\[\033[1;36m\]\$ "
+export PS1="\n\[\033[1;31m\]\u\[\033[35m\]@\[\033[1;32m\]\h\[\033[1;36m\]\$ "
+source $HOME/.git-completion.bash
--- install.sh
+++ install.sh
@@ -18,6 +18,40 @@ echo "install .gitconfig"
 ln -s $PWD/gitconfig/gitconfig $HOME/.gitconfig
 fi
 
+if [ -f $HOME/.git-completion.bash ];then
+echo "mv .git-completion.bash to .git-completion.bash.old"
+mv $HOME/.git-completion.bash $HOME/.git-completion.bash.old
+ln -s $PWD/gitconfig/git-completion.bash $HOME/.git-completion.bash
+else
+echo "install .git-completion.bash"
+ln -s $PWD/gitconfig/git-completion.bash $HOME/.git-completion.bash
+fi
+
+if [ -f $HOME/.tmux.conf ];then
+echo "mv .tmux.conf to .tmux.conf.old"
+mv $HOME/.tmux.conf $HOME/.tmux.conf.old
+ln -s $PWD/tmux_conf/tmux.conf $HOME/.tmux.conf
+else
+echo "install .tmux.conf"
+ln -s $PWD/tmux_conf/tmux.conf $HOME/.tmux.conf
+fi
+
+if [ -f $HOME/.bash_aliases ];then
+echo "mv .bash_aliases to .bash_aliases.old"
+mv $HOME/.bash_aliases $HOME/.bash_aliases.old
+ln -s $HOME/soft_config/bash_aliases $HOME/.bash_aliases
+else
+echo "install .bash_aiases"
+ln -s $HOME/soft_config/bash_aliases $HOME/.bash_aliases
+fi
+
+if [ -d $HOME/.vim/bundle/vundle ];then
+echo "vundle had cloned before, pass"
+else
+git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
+fi
+
+
 if [ -f $HOME/.tmux.conf ];then
 echo "mv .tmux.conf to .tmux.conf.old"
 mv $HOME/.tmux.conf $HOME/.tmux.conf.old
