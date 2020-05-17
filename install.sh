5c8b8fdab21606319d79c5312b36d5066b686040
pydog <pydogwu@gmail.com>
2020-05-17 00:47:42 +0000

    update install.sh for repeated cmd

--- install.sh
+++ install.sh
@@ -50,29 +50,3 @@ echo "vundle had cloned before, pass"
 else
 git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
 fi
-
-
-if [ -f $HOME/.tmux.conf ];then
-echo "mv .tmux.conf to .tmux.conf.old"
-mv $HOME/.tmux.conf $HOME/.tmux.conf.old
-ln -s $PWD/tmux_conf/tmux.conf $HOME/.tmux.conf
-else
-echo "install .tmux.conf"
-ln -s $PWD/tmux_conf/tmux.conf $HOME/.tmux.conf
-fi
-
-if [ -f $HOME/.bash_aliases ];then
-echo "mv .bash_aliases to .bash_aliases.old"
-mv $HOME/.bash_aliases $HOME/.bash_aliases.old
-ln -s $HOME/soft_config/bash_aliases $HOME/.bash_aliases
-else
-echo "install .bash_aiases"
-ln -s $HOME/soft_config/bash_aliases $HOME/.bash_aliases
-fi
-
-if [ -d $HOME/.vim/bundle/vundle ];then
-echo "vundle had cloned before, pass"
-else
-git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
-fi
-
